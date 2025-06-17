d48a68403c1da1e5d2f27372ed60c731d78644d3
Hari <49642836+kris175@users.noreply.github.com>
2025-06-17 18:59:55 +1000

    Updated MapperConfig to account for nested objects

--- src/main/java/com/dev/hari/database_jdbc/config/MapperConfig.java
+++ src/main/java/com/dev/hari/database_jdbc/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package com.dev.hari.database_jdbc.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,8 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return mapper;
     }
 }
